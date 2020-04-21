7cc73112592e892d111b0433a9ac52b4e2813fdb
Philip Zerull <przerull@gmail.com>
2020-04-21 02:13:19 -0400

    got the collections to build using conan.
    There are a few rough edges:
        - there are a bunch of warnings for CMake variables not used
        - we're using self.run instead of a cmake helper method for
          running the tests.
        - the report only shows as running a single test
        - the test is shown as passing if the executable's return code is zero
        - the fructose library isn't packaged using Conan.  It probably
          wouldn't be hard to add, but for now I'm considering using
          gtest
        - no coverage info yet

---
+++ conanfile.py
@@ -0,0 +1,28 @@
+from conans import ConanFile, CMake, tools
+
+
+class CollectionsConan(ConanFile):
+    name = "Collections"
+    version = "0.1"
+    license = "<Put the package license here>"
+    author = "Philip Zerull <przerull@gmail.com>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Collections here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    no_copy_source=True
+    exports_sources = "src/*", "CMakeLists.txt", "test.cpp"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        if tools.get_env("CONAN_RUN_TESTS", True):
+            self.run('ctest --output-on-failure')
+
+
+    def package(self):
+        self.copy("*.h")
+
+    def package_id(self):
+        self.info.header_only()
